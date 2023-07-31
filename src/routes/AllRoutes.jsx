1b4ae6c8a864557d9c9627e82e0934f7419a75e6
Divyesh Bhadrka <p.bhadrka7@gecg28.ac.in>
2023-07-31 13:36:49 +0530

    Routing implemented.

---
+++ src/routes/AllRoutes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, About, NotFound } from "../pages/index.js";
+
+const AllRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AllRoutes;
