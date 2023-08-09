b5d10a08e0ef1d11dc1d93d49e032b2a619565d0
Divyesh Bhadrka <p.bhadrka7@gecg28.ac.in>
2023-08-09 12:11:49 +0530

    Updated card component with book description.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,42 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
 import "./card.css";
+import { ToastContainer, toast } from "react-toastify";
 
 export default function Card(props) {
-  const [isActive, setActive] = useState("Hot");
-
   const addToCart = () => {
     console.log(props.name);
     console.log(props.price);
-    alert("Your " + props.name + " is added to cart.");
+    //   alert("Your " + props.name + " is added to cart.");
+    toast(props.name + " added to cart.");
   };
   return (
     <>
       {/* {console.log("this is card")} */}
+
       <div className="card">
         <div className="card-container">
           <div className="cardImage">
             <img src={props.path} alt="lll" />
+            <p>{props.category}</p>
           </div>
-          {/* <div className="coffeeInfo">
-          </div> */}
           <div className="bookInfo">
             <p>
               <u>{props.name}</u>
             </p>
-            <p>{props.category}</p>
-            {/* <p id="bookDescription">{props.description}</p> */}
+            <span id="bookDescription">{props.description}</span>
             <div className="priceAndCart">
-              <p>Price: Rs.{props.price}</p>
-              <img
-                width="35"
-                height="35"
-                src="https://img.icons8.com/external-obivous-color-kerismaker/48/external-cart-ecommerce-color-obivous-color-kerismaker.png"
-                alt="cart"
-                onClick={addToCart}
-              />
+              <p>Rs.{props.price}</p>
+              <div className="addToCart" onClick={addToCart}>
+                Add to Cart
+                <img
+                  width="35"
+                  height="35"
+                  src="https://img.icons8.com/external-obivous-color-kerismaker/48/external-cart-ecommerce-color-obivous-color-kerismaker.png"
+                  alt="cart"
+                />
+              </div>
             </div>
           </div>
         </div>
+        <ToastContainer position="top-center" autoClose={1300} />
       </div>
     </>
   );
